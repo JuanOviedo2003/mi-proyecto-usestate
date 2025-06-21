638676954456b119e5ea0384ade7d4b0fb0e4f70
JuanOviedo2003 <oviedodiazjuanjose@gmail.com>
2025-06-21 15:33:37 -0500

    feat: better visibility component Contador

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div className='flex flex-col items-center justify-center'>
-        <h1 className='absolute top-10 left-0 text-4xl font-bold text-center bg-amber-300 text-amber-50 p-5'>Ejercicios con UseState</h1>
+        <h1 className='absolute top-5 left-0 text-4xl font-bold text-center bg-amber-300 text-amber-50 p-5'>Ejercicios con UseState</h1>
         <div className='bg-amber-200 h-screen w-[90vw] flex items-center justify-center'>
           <Contador />
         </div>
